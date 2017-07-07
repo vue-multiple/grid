aa00934f0385f88f6ee14affff4ada662fec9bbf
savoygu <saifeizhoukou@163.com>
2017-07-07 16:40:08 +0800

    Update gh-pages.sh & package.json scripts

--- build/gh-pages.sh
+++ build/gh-pages.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-git subtree push --prefix=gh-pages origin gh-pages
+npm run demo:prepublish
+cd gh-pages
+git init
+git add -A
+git commit -m 'update gh-pages'
+git push -f git@github.com:vue-multiple/grid.git master:gh-pages
